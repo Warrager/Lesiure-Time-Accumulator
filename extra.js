65da4f6d6f9d81f366467286a20c15e9578ee13f
Kazuya Honda <honda.r.kazuya@gmail.com>
2016-12-01 03:26:14 +0900

    Alter Added

--- extra.js
+++ extra.js
@@ -1,17 +1,31 @@
 
-//Checks if inputted value is an int
-var num_arr = ['1','2','3','4','5','6','7','8','9','0'];
-$('input').on('input', function(){
-    var input = $(this).val();
-    if (input.length > 2){
-        $(this).val(input.slice(input.length - 2));
+function time_formatter(seconds){
+    var negative = "";
+    if (seconds < 0){
+        seconds *= -1;
+        negative = "-";
     }
-    for (var x = 0, y = input.length; x < y; x++){
-        if (num_arr.indexOf(input[x]) === -1){
-            $(this).val("");
-            //return;
-        }
+    var hour = 0, minute = 0, second;
+    if (seconds / 3600 >= 1){
+        hour = Math.floor(seconds / 3600);
+        seconds %= 3600;
     }
-    var x = parseInt($('#ratio-break').val());
-    var y = parseInt($('#ratio-work').val());
-});
+    if (seconds / 60 >= 1){ 
+        minute = (Math.floor(seconds / 60));
+        seconds %= 60;
+    }
+
+    minute = padder(minute.toString(), 2);   
+    second = padder(seconds.toString(), 2);
+    return (negative + hour + ":" + minute + ":" + second);
+}
+
+
+function padder(str, limit){
+    if (str.length === limit){
+        return str;
+    }
+    else {
+        return padder("0"+str, limit);
+    }
+}
